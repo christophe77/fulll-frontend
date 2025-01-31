95bc2e53b109dbddfe0f98ed6ab9903b3af96371
christophe77 <chrisb@gmx.fr>
2025-01-31 20:34:07 +0100

    feat : search response container

--- src/components/SearchResponseContainer/SearchResponseContainer.tsx
+++ src/components/SearchResponseContainer/SearchResponseContainer.tsx
@@ -1,11 +1,17 @@
-// import useSearchResponseContainer from './useSearchResponseContainer';
+import useSearchResponseContainer from './useSearchResponseContainer';
 import styles from './SearchResponseContainer.module.css';
+import UserCard from '../UserCard/UserCard';
 
-export default function SearchResponseContainer(){
-    return <div className={styles.container}>
-        <h2 className={styles.title}>Results</h2>
-        <div className={styles.cardList}>
-
-        </div>
-    </div>
+export default function SearchResponseContainer() {
+	const { githubUsers } = useSearchResponseContainer();
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.title}>Results</h2>
+			<div className={styles.cardList}>
+				{githubUsers?.map((user) => (
+					<UserCard key={user.id} user={user} />
+				))}
+			</div>
+		</div>
+	);
 }
